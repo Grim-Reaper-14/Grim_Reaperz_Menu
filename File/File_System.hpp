2df28a940e66905120f2ceead903d1a3897a086f
Tony12349 <156120260+Tony12349@users.noreply.github.com>
2025-07-02 10:03:41 -0400

    Update File_System.hpp
    Updating File_System

--- File/File_System.hpp
+++ File/File_System.hpp
@@ -1,47 +1,78 @@
-#ifndef FILE_SYSTEM_HPP  
-#define FILE_SYSTEM_HPP  
+#ifndef FILE_SYSTEM_HPP
+#define FILE_SYSTEM_HPP
 
-#include <string>  
-#include <thread>  
-#include <mutex>  
-#include <condition_variable>  
-#include <queue>  
-#include <fstream>  
-#include <filesystem>  
+#include <string>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <fstream>
+#include <filesystem>
+#include <atomic>
+#include <unordered_map>
 
-namespace fs = std::filesystem;  
+namespace fs = std::filesystem;
 
-class FileSystem {  
-private:  
-    std::string appName;  
-    size_t maxLogSize;  
-    std::string appDir;  
-    std::string backupDir;  
-    std::string logFile;  
-    bool isMonitoring;  
-    std::thread monitorThread;  
-    std::mutex logMutex;  
-    std::condition_variable monitorCv;  
-    std::queue<std::string> logQueue;  
+class FileSystem {
+public:
+    enum class LogLevel { INFO, WARN, ERROR };
 
-    void monitorDirectory();  
-    void processLogQueue();  
-    bool needsLogRotation();  
-    void rotateLogFile();  
+    FileSystem(const std::string& appName, size_t maxLogSize, const std::string& logFileBaseName = "log");
+    ~FileSystem();
 
-public:  
-    FileSystem(const std::string& appName, size_t maxLogSize);  
-    ~FileSystem();  
+    FileSystem(const FileSystem&) = delete;
+    FileSystem& operator=(const FileSystem&) = delete;
+    FileSystem(FileSystem&&) = delete;
+    FileSystem& operator=(FileSystem&&) = delete;
 
-    std::string getAppDataPath();  
-    bool initialize();  
-    bool createFile(const std::string& filename, const std::string& content);  
-    std::string readFile(const std::string& filename);  
-    bool backupFiles();  
-    void startMonitoring();  
-    void stopMonitoring();  
-    void log(const std::string& message); // Public method declaration for logging
-};  
+    std::string getAppDataPath() const;
+    bool initialize();
+    bool createFile(const std::string& filename, const std::string& content);
+    std::string readFile(const std::string& filename);
+    bool backupFiles();
+
+    void startMonitoring();
+    void stopMonitoring();
+
+    void startAsyncBackup();
+    void stopAsyncBackup();
+
+    void log(const std::string& message, LogLevel level = LogLevel::INFO);
+
+private:
+    const std::string appName_;
+    const size_t maxLogSize_;
+    fs::path appDataDir_;
+    fs::path backupDir_;
+    fs::path logFilePath_;
+
+    std::string baseLogFileName_;
+    std::string activeLogFileName_;
+
+    std::atomic<bool> isMonitoring_{false};
+    std::atomic<bool> isBackingUp_{false};
+
+    std::thread monitorThread_;
+    std::thread backupThread_;
+    std::mutex logMutex_;
+    std::condition_variable monitorCv_;
+    std::queue<std::string> logQueue_;
+
+    std::unordered_map<std::string, fs::file_time_type> fileTimes_;
+
+    void monitorDirectory();
+    void processLogQueue();
+    bool needsLogRotation();
+    void rotateLogFile();
+
+    void compressOldLogs();
+    void watchFilesForChanges();
+
+    bool createDirectoriesIfNotExist(const fs::path& path);
+    std::string currentTimestamp() const;
+    std::string currentDate() const;
+    std::string logLevelToString(LogLevel level) const;
+    std::string toJsonLog(const std::string& timestamp, const std::string& level, const std::string& message) const;
+};
 
 #endif // FILE_SYSTEM_HPP
-
